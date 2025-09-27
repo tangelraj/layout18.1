1c7d5651f7217564196abd6c6f653b325083ccb8
tangelraj <tangelraj@gmail.com>
2025-09-28 02:32:20 +0530

    Update settings.py

--- petpalooza/settings.py
+++ petpalooza/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pzv2&%0jbesp26ra9-_x^jo)2ib-xdrp%qk_%43zxf=phv)7@r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
